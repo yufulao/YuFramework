5d7385ffff8d53d63769194d3753a6de001d4a27
yufulao <3493942038@qq.com>
2025-02-11 20:03:08 +0800

    移除无关事件名

--- Assets/Scripts/Core/Manager/EventManager/EventName.cs
+++ Assets/Scripts/Core/Manager/EventManager/EventName.cs
@@ -12,38 +12,7 @@ namespace Yu
         OnMouseRightClick,
         OnHoldBegin,
         OnHoldEnd,
-        ChangeScene,//场景切换时，1.无参。2.(string)角色出生点id
-        OnLanguageChanged,
-        
-        OnExitGame,//退出游戏时
-        OnPauseGame,//暂停游戏时
-        OnUnpauseGame,//继续游戏时
-        OnPauseViewClose,//暂停界面关闭时
-
-        OnLoadGame,//加载存档时
-        OnSaveGame,//保存当前存档时
-        //OnLastSaveGameChange,//最新存档变化时
-        
-        InventoryContentChange,//背包物品变化
-        OnGameTimeRunUpdate,//(float->本帧流逝的second)，Update调用
-        OnGameTimeMinuteChange,//(无参数)
-        OnGameTimeHourChange,//(无参数)
-        OnGameTimeUnitChange,//(无参数)
-        OnGameTimePeriodChange,//(无参数)
-        OnGameTimeDayChange,//(无参数)
-        OnGameTimeYearChange,//(无参数)
-        OnCharacterEnergyChange,//(int,float)角色体力值比率变化时，(当前value，当前ratio)
-        OnCharacterHungryStateChange,//(bool)角色饥饿状态变化时，true：转为饥饿状态，false：转为饱腹状态
-        OnCharacterHungerChange,//(float,float)角色饥饿值比率变化时，(当前value，当前ratio)
-        OnCharacterMoveSpeedChange,//(float)角色移动速度变化时
-        OnCharacterCurrentExpInspirationChange,//(int)角色灵感经验变化时
-        OnCharacterCurrentLevelInspirationChange,//(int)角色等级变化时
-        
-        OnCharacterDie,//角色死亡时
-        OnCharacterSleepStart,//角色睡觉时
-        OnCharacterSleepEnd,//(CharacterSleepType)角色睡觉时
-        OnSkipTimeForCharacterSleep,//(GameTime->跳过的时间)角色睡觉跳过时间时
-        
+        ChangeScene,
         OnGmOpen, //打开GM界面时
     }
 }
